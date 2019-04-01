ebef75a198217539091dd582dc8537a6e5930124
Nienkemart1 <47739200+Nienkemart1@users.noreply.github.com>
2019-04-01 09:09:03 +0200

    Update uitprobeer.js

--- uitprobeer.js
+++ uitprobeer.js
@@ -1,7 +1,8 @@
 function getStadAPIdata() {
   fetch('https://ckan.dataplatform.nl/api/3/action/datastore_search?resource_id=4cd93a3f-7d2d-4758-af0d-d0aab05fc67a&limit=5')
-   
-
+   //hier heb geprobeert een derde Api key te implenmenteren. Helaas is dit niet gelukt doordat de kaart hierdoor dan verdween.
+  //deze api key  moes per stad een  kunstwerk tonen. Mijn bedoeling was om in combinatie met de kaart hiervan gebruik te maken.
+  //Wanneer de gebruiker op een plaats klikt dan verschijnt er een alert
 var data = {
     resource_id: '4cd93a3f-7d2d-4758-af0d-d0aab05fc67a', // the resource id
     limit: 5, // get 5 results
