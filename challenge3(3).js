d31d2fb5f24a7a1a5de518dda19022f33f224205
Nienkemart1 <47739200+Nienkemart1@users.noreply.github.com>
2019-04-02 15:22:21 +0200

    Update challenge3(3).js

--- challenge3(3).js
+++ challenge3(3).js
@@ -1,15 +1,16 @@
 function getStadAPIdata() {
     fetch('https://ckan.dataplatform.nl/api/action/datastore_search?resource_id=a90c1897-6c48-455f-a474-98097c43d1a7')
+    //
 
     .then(function(response) {
         return response.json();
     })
 
     .then(function(response) {
-    	var text = '';
+    	var text = ''; //lege variabele
       console.log(response.result.records);
       for (var i =0; i < response.result.records.length; i++) {
-        text =  (response.result.records[i].STRAAT +'<br>')
+        text ==  (response.result.records[i].STRAAT +'<br>')
       }
       document.getElementById('buurt').innerHTML = text;
 
